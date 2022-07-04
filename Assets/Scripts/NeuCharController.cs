e851eccad1ad90bf2eebff4a994b9c07a3eeaf11
ekrem.akpinar <ekrem.akpinar@hotmail.de>
2022-07-04 10:06:55 +0200

    Kommentar "Neuer Text" aus NeuCharController gelöscht

--- Assets/Scripts/NeuCharController.cs
+++ Assets/Scripts/NeuCharController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class NeuCharController : MonoBehaviour
 {
-    // Neuer Text
     // Deklaration Rigidbody des Characters
     private Rigidbody rb;
     // Deklaration Animation des Characters
